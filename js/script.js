ba46fe0f55a730b204fa033e28181bea7da15798
catherine <catherine.nicloux@hotmail.fr>
2020-03-26 11:49:11 +0100

    ajout lien retour Portefolio et Github

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function accesForum() {
 	document.write("<h1>Bienvenue sur le forum !! Page en construction !!!</h1>");
+	// document.write("<h2>toto</h2>");
+	document.write("<a href="https:\/\/catherine57.github.io\/SiteBlog\/"><h3>Retourner sur le blog<h3> </a>");
+
+// document.getElementById("article1").innerHTML = "<img src='something' onmouseover='change(&quot;ex1&quot;)' />";
 };
 
