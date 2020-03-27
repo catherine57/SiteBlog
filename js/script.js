88729368fcb575ef05b46628bbbf8255e8815673
catherine <catherine.nicloux@hotmail.fr>
2020-03-27 13:51:35 +0100

    forum redirection

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 function accesForum() {
 	document.write("<h1>Bienvenue sur le forum !! Page en construction !!!</h1>");
-	// document.write("<h2>toto</h2>");
-	document.write("<a href="https:\/\/catherine57.github.io\/SiteBlog\/"><h3>Retourner sur le blog<h3> </a>");
-
-// document.getElementById("article1").innerHTML = "<img src='something' onmouseover='change(&quot;ex1&quot;)' />";
+	
+	var urlActuel = window.location;
+	alert("URL actuelle est : " + urlActuel.toString() );
+	
+	alert("Retournons à la page d'accueil");
+		
+	window.location = "https://catherine57.github.io/SiteBlog/";
 };
 
